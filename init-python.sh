4fbb363e8d11508263b276b4eb63b14cf297a0a7
Freesca <checco1009@gmail.com>
2024-11-29 15:45:31 +0100

    added init script + .env example

---
+++ init-python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+directories=(./*_microservice)
+
+for dir in "${directories[@]}"; do
+	echo "Installing pyenv in $dir"
+	cd "$dir"
+	python3 -m venv pyenv
+	echo "Installint requirements in $dir"
+	pyenv/bin/pip3 install -r requirements.txt
+	echo "Done!"
+	cd ..
+done
