5848536488136b17fef07bdcecb99f756f322553
Sandoramix <sandoramix.dev@gmail.com>
2024-11-30 13:02:01 +0100

    enabled CORS

--- matchmaking_microservice/mtcmkg/settings.py
+++ matchmaking_microservice/mtcmkg/settings.py
@@ -50,13 +50,17 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
     'mtcmkg_api',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
