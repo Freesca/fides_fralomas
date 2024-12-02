7914bb5045c9e1eeffb70d6f242bf21bfd64380a
Freesca <checco1009@gmail.com>
2024-12-02 19:50:02 +0100

    nothing here

--- init-python.sh
+++ init-python.sh
@@ -4,7 +4,7 @@
 directories=(./*_microservice)
 
 for dir in "${directories[@]}"; do
-	echo "Installing pyenv in $dir"
+	echo "Installing venv in $dir"
 	cd "$dir"
 	python3 -m venv venv
 	echo "Installint requirements in $dir"
